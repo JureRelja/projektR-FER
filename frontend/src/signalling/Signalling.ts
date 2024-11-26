7d0c5d1fc5bc276ef9954c51349b088688041578
JureRelja <jurereljanovic99@gmail.com>
2024-11-26 13:53:38 +0100

    abstracted the websockets logic into just one type of signalling server impl

---
+++ frontend/src/signalling/Signalling.ts
@@ -0,0 +1,24 @@
+import { Participant } from "../types/Participant";
+
+export interface Signalling {
+    makeAnswer(answer: RTCSessionDescriptionInit, callerId: string | number): void;
+
+    startCall(offer: RTCSessionDescriptionInit): void;
+
+    gettingCalled(
+        setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        createAndSendAnswer: (
+            data: { offer: RTCSessionDescriptionInit; socketId: string },
+            setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+            setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        ) => void,
+    ): void;
+
+    answerMade(peerConnection: RTCPeerConnection): void;
+
+    getRoomData(
+        setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+    ): void;
+}
---
+++ frontend/src/signalling/websocket/SocketSignalling.ts
@@ -0,0 +1,53 @@
+import { Socket } from "socket.io-client";
+import { Signalling } from "../Signalling";
+import { Participant } from "../../types/Participant";
+
+export class WebSocketSignalling implements Signalling {
+    private socket: Socket;
+
+    constructor(socket: Socket) {
+        this.socket = socket;
+    }
+    gettingCalled(
+        setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        createAndSendAnswer: (
+            data: { offer: RTCSessionDescriptionInit; socketId: string },
+            setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+            setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        ) => void,
+    ): void {
+        this.socket.on("gettingCalled", (data) => {
+            if (data.socketId != this.socket.id) {
+                console.log("You are getting called by socketId", data.socketId);
+                createAndSendAnswer(data, setParticipant1, setParticipant2);
+            }
+        });
+    }
+
+    answerMade(peerConnection: RTCPeerConnection): void {
+        this.socket.on("answerMade", async (data) => {
+            if (data.caleeSocketId != this.socket.id) {
+                await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+            }
+        });
+    }
+
+    getRoomData(
+        setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+        setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
+    ): void {
+        this.socket.on("roomData", (data: Participant[]) => {
+            setParticipant1(data[0]);
+            setParticipant2(data[1]);
+        });
+    }
+
+    startCall(offer: RTCSessionDescriptionInit): void {
+        this.socket.emit("makeCall", { offer });
+    }
+
+    makeAnswer(answer: RTCSessionDescriptionInit, callerId: string | number) {
+        this.socket.emit("makeAnswer", { answer, to: callerId });
+    }
+}

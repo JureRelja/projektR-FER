3abb099673d29135f8d22a61096e911abfb5261a
JureRelja <jurereljanovic99@gmail.com>
2024-11-26 20:09:14 +0100

    removed unnecessarry methods from signalling server

--- frontend/src/signalling/Signalling.ts
+++ frontend/src/signalling/Signalling.ts
@@ -1,20 +1,6 @@
 import { Participant } from "../types/Participant";
 
 export interface Signalling {
-    makeAnswer(answer: RTCSessionDescriptionInit, callerId: string | number): void;
-
-    startCall(offer: RTCSessionDescriptionInit, roomId: number): void;
-
-    gettingCalled(
-        setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-        setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-        createAndSendAnswer: (
-            data: { offer: RTCSessionDescriptionInit; socketId: string },
-            setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-            setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-        ) => void,
-    ): void;
-
     answerMade(peerConnection: RTCPeerConnection): void;
 
     getRoomData(
--- frontend/src/signalling/websocket/SocketSignalling.ts
+++ frontend/src/signalling/websocket/SocketSignalling.ts
@@ -8,22 +8,6 @@ export class WebSocketSignalling implements Signalling {
     constructor(socket: Socket) {
         this.socket = socket;
     }
-    gettingCalled(
-        setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-        setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-        createAndSendAnswer: (
-            data: { offer: RTCSessionDescriptionInit; socketId: string },
-            setParticipant1: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-            setParticipant2: React.Dispatch<React.SetStateAction<Participant | undefined>>,
-        ) => void,
-    ): void {
-        this.socket.on("gettingCalled", (data) => {
-            if (data.socketId != this.socket.id) {
-                console.log("You are getting called by socketId", data.socketId);
-                createAndSendAnswer(data, setParticipant1, setParticipant2);
-            }
-        });
-    }
 
     answerMade(peerConnection: RTCPeerConnection): void {
         this.socket.on("answerMade", async (data) => {
@@ -44,13 +28,5 @@ export class WebSocketSignalling implements Signalling {
             setParticipant1(data[0]);
             setParticipant2(data[1]);
         });
-    }
-
-    startCall(offer: RTCSessionDescriptionInit, roomId: number): void {
-        this.socket.emit("makeCall", { offer, roomId });
-    }
-
-    makeAnswer(answer: RTCSessionDescriptionInit, callerId: string | number) {
-        this.socket.emit("makeAnswer", { answer, to: callerId });
     }
 }

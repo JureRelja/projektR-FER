145578deae184418c576f37533e8e15cff632579
JureRelja <jurereljanovic99@gmail.com>
2024-12-22 10:59:56 +0100

    updated component

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 export default function Index({ label, onClick }: { label: string; onClick: (event: React.MouseEvent<HTMLButtonElement>) => void }) {
     return (
         <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-fit focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 hover: shadow-md"
+            className="bg-blue-500 min-w-fit hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-fit focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 hover: shadow-md"
             onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
                 onClick(event);
             }}>
